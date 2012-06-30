501599110f322746a6d038ca1987001ec41dfc2a
Alex Barth <alex@developmentseed.org>
2012-06-30 15:35:06 -0400

    Layer switcher.

--- site.js
+++ site.js
@@ -7,7 +7,6 @@ $(function() {
         'villeda.map-knrpf5da',
         'villeda.map-iaqy28df'
     ];
-    var map;
     var layerTemplate = $('#layer-switcher ul').html();
     $('#layer-switcher ul').empty();
 
@@ -21,27 +20,48 @@ $(function() {
         return str.replace(/[^a-z,A-Z,0-0,-]/g, '-');
     };
 
-    var loadMap = function(tilejson) {
-        map = new MM.Map('map',
+    var buildMap = function(tilejson) {
+        // Map
+        var map = new MM.Map('map',
         new wax.mm.connector(tilejson));
         map.setCenterZoom(new MM.Location(tilejson.center[0],
             tilejson.center[1]),
             tilejson.center[2]);
-
         wax.mm.zoomer(map).appendTo(map.parent);
-        wax.mm.interaction()
+
+        // Interaction
+        var interaction = wax.mm.interaction()
             .map(map)
             .tilejson(tilejson)
             .on(wax.tooltip().animate(true).parent(map.parent).events());
-        wax.mm.legend(map, tilejson).appendTo(map.parent);
 
-        $('#attribution').empty().append(tilejson.attribution);
-        $('#layer-switcher li').removeClass('active');
-        $('#layer-switcher li#' + cssSafe(tilejson.id)).addClass('active');
-    };
+        // Legend
+        var legend = wax.mm.legend(map, tilejson).appendTo(map.parent);
 
-    var updateMap = function(tilejson) {
-        // todo
+        // Update UI from map info
+        var updateUI = function(tilejson) {
+            $('#attribution').empty().append(tilejson.attribution);
+            $('#layer-switcher li').removeClass('active');
+            $('#layer-switcher li#' + cssSafe(tilejson.id)).addClass('active');
+        };
+        updateUI(tilejson);
+
+        // Return handler for updating the map.
+        return function(tilejson) {
+            interaction && interaction.remove();
+            map.setLayerAt(0, new wax.mm.connector(tilejson));
+            interaction = wax.mm
+                .interaction()
+                .map(map)
+                .tilejson(tilejson)
+                .on(wax.tooltip().animate(true).parent(map.parent).events());
+            if (legend) {
+                $(legend.element()).css('opacity', 0);
+                legend.content(tilejson);
+                $(legend.element()).animate({opacity: 1}, 500);
+            }
+            updateUI(tilejson);
+        };
     };
 
     _.each(layers, function(layer, i){
@@ -53,17 +73,22 @@ $(function() {
             layerInfo.push(tilejson);
             $('.title', el).empty().append(tilejson.name);
             $('.description', el).empty().append(tilejson.description);
-            i == 0 && loadMap(tilejson);
+
+            // Load the first map and attach event handlers.
+            if (i == 0) {
+                var updateMap = buildMap(tilejson);
+                $('#layer-switcher li .title').click(function() {
+                    var id = $(this).parent().attr('id');
+                    _.any(layerInfo, function(tilejson) {
+                        if (id != cssSafe(tilejson.id)) return false;
+                        updateMap(tilejson);
+                        return true;
+                    });
+                    return false;
+                });
+            }
         });
     });
 
-    $('#layer-switcher li .title').click(function() {
-        var id = $(this).attr('id');
-        _.any(layerInfo, function(info) {
-            if (id != info.id) return false;
-            updateMap(id);
-            return true;
-        });
-        return false;
-    });
+    
 });
