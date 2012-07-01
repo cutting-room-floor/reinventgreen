6d3c7ab62407acf578d4a5df508a4e5d9f8b9ae1
Alex Barth <alex@developmentseed.org>
2012-07-01 16:30:38 -0400

    Fix embeds.

--- site.js
+++ site.js
@@ -70,8 +70,8 @@ $(function() {
                 $('#layer-switcher li .title').click(function() {;
                     var id = $(this).parent().attr('id');
                     updateMap(layers[id]);
-                    $('.share .tilejson textarea').empty().text(url(id));
-                    $('.share .embed textarea').empty().text("<iframe style='background-color: #000' width='500' height='300' frameBorder='0' src='http://a.tiles.mapbox.com/v3/" + id + ".html#11,40.7010,-74.0137'></iframe>");
+                    $('.share .tilejson textarea').empty().text(url(layers[id].id));
+                    $('.share .embed textarea').empty().text("<iframe style='background-color: #000' width='500' height='300' frameBorder='0' src='http://a.tiles.mapbox.com/v3/" + layers[id].id + ".html#11,40.7010,-74.0137'></iframe>");
                     return false;
                 });
             }
