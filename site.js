bf7f21b55e1d52dfa8419bd201435475dd8590b1
Ian Ward <ianshward@gmail.com>
2013-09-16 18:36:43 -0400

    https resources

--- site.js
+++ site.js
@@ -80,7 +80,7 @@ $(function() {
         $('.share .tilejson textarea').empty().text(tileUrl(id));
         $('.share .embed textarea').empty().text(
             "<iframe style='background-color: #000' width='500' height='300' " +
-            "frameBorder='0' src='http://a.tiles.mapbox.com/v3/" + id + ".html" +
+            "frameBorder='0' src='https://a.tiles.mapbox.com/v3/" + id + ".html" +
             "#11,40.7010,-74.0137'></iframe>"
         );
         $('.modal.share').stop().fadeIn(100);
